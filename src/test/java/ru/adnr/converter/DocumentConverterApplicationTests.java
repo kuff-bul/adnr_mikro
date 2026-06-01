013b3f8ce138a8dd635e892b90c5a99a114db885
Bulat <kusaeff.b@gmail.com>
2026-06-01 12:49:53 +0400

    feature(micro-service-1): база

---
+++ src/test/java/ru/adnr/converter/DocumentConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.adnr.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DocumentConverterApplicationTests {
+
+    @Test
+    void applicationClassExists() {
+        assertThat(DocumentConverterApplication.class).isNotNull();
+    }
+}
